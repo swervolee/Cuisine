20019f87be5dee29713736a92567333fe4dd5309
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-03-08 20:22:24 +0300

    Updated /home/rhlee/my_projects/Holberton/Cuisine/.git/COMMIT_EDITMSG

--- web_static/scripts/0-script.js
+++ web_static/scripts/0-script.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 
 $(document).ready(function() {
+    console.log("Script loaded");
+
     $(".recipe-creation").on("click", function() {
+        console.log("Clicked");
         $(this).toggleClass("show");
     });
 });
