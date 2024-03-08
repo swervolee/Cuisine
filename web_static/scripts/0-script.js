76c07a9f11ef11e91db2496c829f6c8ac774679c
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-03-08 18:31:17 +0300

    Updated /home/rhlee/my_projects/Holberton/Cuisine/.git/COMMIT_EDITMSG

--- web_static/scripts/0-script.js
+++ web_static/scripts/0-script.js
@@ -1,17 +1,8 @@
 #!/usr/bin/node
 
 $(document).ready(function() {
-  // Function to add numbered input fields for ingredients
-  $('#addIngredient').click(function(e) {
-    e.preventDefault();
-    var count = $('#ingredients-list div').length + 1;
-    $('#ingredients-list').append('<div><label for="ingredient_' + count + '">Ingredient ' + count + ':</label><input type="text" id="ingredient_' + count + '" name="ingredient_' + count + '" required><br></div>');
-  });
-
-  // Function to add numbered textarea fields for instructions
-  $('#addInstruction').click(function(e) {
-    e.preventDefault();
-    var count = $('#instructions-list textarea').length + 1;
-    $('#instructions-list').append('<div><label for="instruction_' + count + '">Instruction ' + count + ':</label><textarea id="instruction_' + count + '" name="instruction_' + count + '" rows="4" cols="50" required></textarea><br></div>');
-  });
-});
+    $( ".recipe-creation" ).on("click", function() {
+	this.css("display", "block")
+    });
+}
+	
