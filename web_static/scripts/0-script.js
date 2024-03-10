4ee095fbdbc87ada1968a408397401038dcb38ab
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-03-10 12:41:26 +0300

    Updated /home/rhlee/my_projects/Holberton/Cuisine/.git/COMMIT_EDITMSG

--- web_static/scripts/0-script.js
+++ web_static/scripts/0-script.js
@@ -88,8 +88,7 @@ $(document).ready(function() {
 	$(".recipe-creation h3").trigger("click");
     });
 
-    $(".recipe-creation h3").trigger("click");
-
+ 
 
     /*Hide the recipe creation form when area outside of it
       is clicked*/
