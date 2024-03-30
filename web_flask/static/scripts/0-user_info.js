870c72fa880f72ae49e3024cac0a1d407bac1106
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-03-30 19:25:51 +0300

    Javascript

--- web_flask/static/scripts/0-user_info.js
+++ web_flask/static/scripts/0-user_info.js
@@ -3,13 +3,12 @@ var user = {};
 $.ajax({
     url: "http://0.0.0.0:5001/status",
     type: "GET",
-    dataType: "json" // corrected datatype to dataType
 }).done(function (json) {
     if (json.status === "logged") {
         console.log("User is logged in and id is " + json.id);
         
         $.ajax({
-            url: "http://0.0.0.0:5000/api/v1/users/" + json.id, // corrected URL
+            url: "http://0.0.0.0:5000/api/v1/users/" + json.id,
             dataType: "json",
             type: "GET"
         }).done(function (userJson) {
