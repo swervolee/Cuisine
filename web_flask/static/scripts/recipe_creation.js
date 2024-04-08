bc8691d7b646ef9edd3f3ba0667fba16ee900299
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-04-08 23:31:26 +0300

    .

--- web_flask/static/scripts/recipe_creation.js
+++ web_flask/static/scripts/recipe_creation.js
@@ -1,13 +1,34 @@
 
 $(function() {
-    $("#txtarea").on("input", function () {
+    $("#txtarea, .instruction textarea").on("input", function() {
         if (this.scrollHeight > this.clientHeight) {
             $(this).css({
                 "height": this.scrollHeight + "px",
             });
         } else {
             $(this).css({
-                "height": "auto",
+                "height": $(this).scrollHeight + "px",
+            });
+        }
+
+        // Load any saved value from localStorage
+        var savedValue = localStorage.getItem('textareaValue');
+        if (savedValue) {
+            $("#txtarea, .instruction textarea").val(savedValue);
+        }
+    });
+
+    // Save the value to localStorage whenever it changes
+    $("#txtarea, .instruction textarea").on("input", function() {
+        localStorage.setItem('textareaValue', $(this).val());
+
+        if (this.scrollHeight > this.clientHeight) {
+            $(this).css({
+                "height": this.scrollHeight + "px",
+            });
+        } else {
+            $(this).css({
+                "height": $(this).scrollHeight + "px",
             });
         }
     });
