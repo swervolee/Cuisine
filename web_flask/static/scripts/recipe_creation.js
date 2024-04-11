68b3c0fd52011068d5eb83ab9af7705d7e5b5b09
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-04-11 23:11:09 +0300

    Api advanced

--- web_flask/static/scripts/recipe_creation.js
+++ web_flask/static/scripts/recipe_creation.js
@@ -1,35 +1,107 @@
 
+
 $(function() {
-    $("#txtarea, .instruction textarea").on("input", function() {
+    var savedValue = localStorage.getItem('textareaValue');
+    if (savedValue) {
+        $("#txtarea").val(savedValue);
+    }
+
+    var title = localStorage.getItem("title");
+    if (title) {
+        $("#title").val(title);
+    }
+
+    $("#title").on("input", function() {
+        localStorage.setItem("title", $(this).val());
         if (this.scrollHeight > this.clientHeight) {
             $(this).css({
                 "height": this.scrollHeight + "px",
             });
         } else {
             $(this).css({
-                "height": $(this).scrollHeight + "px",
+                "height": "auto",
             });
-        }
-
-        // Load any saved value from localStorage
-        var savedValue = localStorage.getItem('textareaValue');
-        if (savedValue) {
-            $("#txtarea, .instruction textarea").val(savedValue);
         }
     });
 
-    // Save the value to localStorage whenever it changes
-    $("#txtarea, .instruction textarea").on("input", function() {
-        localStorage.setItem('textareaValue', $(this).val());
+    var ds = localStorage.getItem("ds");
 
+    if (ds) {
+        $("#ds").val(ds);
+    }
+    
+    $("#ds").on("input", function() {
+        localStorage.setItem("ds", $(this).val());
         if (this.scrollHeight > this.clientHeight) {
             $(this).css({
                 "height": this.scrollHeight + "px",
             });
         } else {
             $(this).css({
-                "height": $(this).scrollHeight + "px",
+                "height": "auto",
             });
         }
+    });
+
+    var notes = localStorage.getItem("notes");
+    if (notes) {
+        $("#notes").val(notes);
+    }
+
+    $("#notes").on("input", function() {
+        localStorage.setItem("notes", $(this).val());
+        if (this.scrollHeight > this.clientHeight) {
+            $(this).css({
+                "height": this.scrollHeight + "px",
+            });
+        } else {
+            $(this).css({
+                "height": "auto",
+            });
+        }
+    });
+
+    var txt = localStorage.getItem("txt");
+    if (txt) {
+        $("#txt").val(txt);
+    }
+
+    $("#txt").on("input", function() {
+        localStorage.setItem("txt", $(this).val());
+        if (this.scrollHeight > this.clientHeight) {
+            $(this).css({
+                "height": this.scrollHeight + "px",
+            });
+        } else {
+            $(this).css({
+                "height": "auto",
+            });
+        }
+    });
+
+    var txtarea = localStorage.getItem("txtarea");
+    if (txtarea) {
+        $("#txtarea").val(txtarea);
+    }
+
+    $("#txtarea").on("input", function() {
+        localStorage.setItem("txtarea", $(this).val());
+        if (this.scrollHeight > this.clientHeight) {
+            $(this).css({
+                "height": this.scrollHeight + "px",
+            });
+        } else {
+            $(this).css({
+                "height": "auto",
+            });
+        }
+    });
+
+
+
+
+    $("header .save").on("click", function(){
+        /*code to send to api goes here*/
+        localStorage.clear();
     });
 });
