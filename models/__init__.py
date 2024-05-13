a9ef0e43ef8549c97823852badd06c608154fc9c
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-05-13 09:24:26 +0300

    Enviroment variable change

--- models/__init__.py
+++ models/__init__.py
@@ -4,11 +4,11 @@ from os import getenv
 
 storage_type = getenv('CUISINE_TYPE_STORAGE')
 
-if storage_type == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
+#if storage_type == "db":
+from models.engine.db_storage import DBStorage
+storage = DBStorage()
+# else:
+#    from models.engine.file_storage import FileStorage
+#    storage = FileStorage()
 
 storage.reload()
